1755a3fd66c28b32f70af7a84bddcba94dfcb5d9
emgniddikur <yoshiharu2580@gmail.com>
2022-06-26 16:10:14 +0900

    add tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod tcp;
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<_>>();
+    let protocol: &str = &args[1];
+    let role: &str = &args[2];
+    match protocol {
+        "tcp" => match role {
+            "server" => {
+                tcp::server::serve();
+            }
+            _ => {}
+        },
+        _ => {}
+    }
 }
